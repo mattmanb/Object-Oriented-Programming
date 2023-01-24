ac79b4a3c13e8f14edb1ebe3da312c5a6b583ec6
Matt Barrington <mattbarrington7@gmail.com>
2023-01-24 16:16:15 -0500

    Commit assignment 1 code

---
+++ assign01/src/main/java/Greetings.java
@@ -0,0 +1,5 @@
+public class Greetings {
+    public static void main(String [] args) {
+        System.out.println("How much wood could a woodchuck chuck if a woodchuck could chuck wood?");
+    }
+}
