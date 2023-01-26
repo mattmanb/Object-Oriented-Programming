cabe523aa6f23195e221a9ec73f9b9c130bb9eaa
Matt Barrington <mattbarrington7@gmail.com>
2023-01-26 17:14:47 -0500

    Assign 1 commit

--- assign01/src/main/java/edu/barrinm/assign01/Greetings.java
+++ assign01/src/main/java/edu/barrinm/assign01/Greetings.java
@@ -2,6 +2,6 @@ package edu.barrinm.assign01;
 
 public class Greetings {
     public static void main(String [] args) {
-        System.out.println("Life before death, strength before weakness, journey before destination");
+        System.out.println("Life before death, strength before weakness, journey before destination.");
     }
 }
