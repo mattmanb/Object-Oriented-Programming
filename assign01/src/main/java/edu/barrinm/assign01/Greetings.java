09fea8b0a564c787b24c2f7e136455d52ffabbc8
Matt Barrington <mattbarrington7@gmail.com>
2023-02-02 17:03:20 -0500

    Assign 1 commit

--- assign01/src/main/java/edu/barrinm/assign01/Greetings.java
+++ assign01/src/main/java/edu/barrinm/assign01/Greetings.java
@@ -2,6 +2,6 @@ package edu.barrinm.assign01;
 
 public class Greetings {
     public static void main(String [] args) {
-        System.out.println("How much wood could a woodchuck chuck if a woodchuck could chuck wood??");
+        System.out.println("Life before death, strength before weakness, journey before destination.");
     }
 }
