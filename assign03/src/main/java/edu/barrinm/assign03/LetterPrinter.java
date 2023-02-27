69ffa01b4af324776c3864197ca898153b97c786
Matt Barrington <mattbarrington7@gmail.com>
2023-02-27 15:55:48 -0500

    Assignment 3 commit; tests aren't perfect yet

--- assign03/src/main/java/edu/barrinm/assign03/LetterPrinter.java
+++ assign03/src/main/java/edu/barrinm/assign03/LetterPrinter.java
@@ -1,4 +1,29 @@
 package edu.barrinm.assign03;
+import java.util.Scanner;
 
 public class LetterPrinter {
+
+    public static Letterhead askForLetterheadData(Scanner input) {
+        String name;
+        char boundaryChar;
+        String [] sloganLines = new String[4]; //string array of size 4
+        System.out.println("Enter business name:");
+        name = input.nextLine(); //reads the business name
+        System.out.println("Enter boundary char:");
+        String sBChar = input.nextLine(); //temporary string for boundary char
+        boundaryChar = sBChar.charAt(0); //gets the first char of sBChar
+        System.out.println("Enter slogan (max 4 lines):");
+        for(int i = 0; i < sloganLines.length; i++) { //reads the next 4 lines of user input
+            sloganLines[i] = input.nextLine();
+        }
+        Letterhead l = new Letterhead(name, sloganLines, boundaryChar); //new Letterhead object
+        return l;
+    }
+
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in); //new Scanner object
+        Letterhead header = askForLetterheadData(input); //new Letterhead object
+        System.out.println("Your new letterhead");
+        System.out.println(header); //prints the Letterhead
+    }
 }
